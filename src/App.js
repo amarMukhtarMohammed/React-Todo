93373fa526650eeb6c2e6adfae2ada11d0c760ad
Amar <amarmukhtar398@gmail.com>
2022-03-29 13:02:59 +0100

    Incorperated mapping and props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import './App.js';
+import Todos from './components/Todos';
 
 class App extends Component {
-  render() {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: '1 st Todo!',
+        completed: false
+      },
+       {
+        id: 2,
+        title: '2nd Todo!',
+        completed: false
+      },
+        {
+        id: 3,
+        title: '3rd Todo!',
+        completed: false
+      }
+    ]
+  }
+  render() {  
     return (
       <div className='App'>
-        <h1>Amar MM</h1>
-      </div>
-    );
+        <Todos todos={this.state.todos} />            {/* passing in the individual todos therefore becoming a prop */}
+        {/* The data comes from the state of app.js and is passed to Todos as props */}
+        {/* mapping = return an array from an array */}
+      </div> 
+    ); 
   }
 }
 
